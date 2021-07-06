b3f84cd7c83353760295b38b40cf2d676050460c
arturexpone <arturexpone@yandex.ru>
2021-07-06 14:05:23 +0300

    add button in admin page

---
+++ BannerManagement/Controller/Adminhtml/Settings/index.php
@@ -0,0 +1,50 @@
+<?php
+namespace M2task\BannerManagement\Controller\Adminhtml\Settings;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'M2task_BannerManagement::bannermanagement';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/bannermanagement_settings_index.xml
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('Banner Management'));
+
+        return $resultPage;
+    }
+}
